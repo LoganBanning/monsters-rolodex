1f02769e49ed1cdb34c4d4f310348d6ec14a51a0
Logan Banning <loganboganb@gmail.com>
2023-02-09 18:17:25 -0700

    set up search and main fetch request

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [],
+      searchField: ''
+      };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then((users) => this.setState(() => {
+        return {monsters: users}
+      },
+      () => {
+        console.log(this.state)
+      }
+      ))
+  }
+
+  render(){
+    const filteredMonsters = this.state.monsters.filter((monster) => {
+      return monster.name.toLowerCase().includes(this.state.searchField);
+    });
+    
+    return (
+      <div className="App">
+      <input className='search-box' type='search' placeholder='Search Monsters' onChange={(event) => {
+        const searchField = event.target.value.toLowerCase();
+        this.setState(() => {
+          return { searchField };
+        })
+      }} />
+        {
+          filteredMonsters.map((monster) => {
+            return <h1 key={monster.id}>{monster.name}</h1>;
+          })
+        }
+      </div>
+    );
+  }
+  }
+
+export default App;
